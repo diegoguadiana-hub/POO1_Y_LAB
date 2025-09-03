fce186447b9361863f78bd77c3c8de62d002fdd9
diegoguadiana-hub <diegoguadiana34@gmail.com>
2025-09-03 08:52:17 -0600

    Práctica3

---
+++ p033_NumerosConsecutivos.java
@@ -0,0 +1,31 @@
+// p033 Numeros Consecutivo 
+// Diego Bernardo Guadiana Gómez
+// Verificar si 3 números son consecutivos y mandar un mensaje confimando si lo son o no
+
+import java.util.Scanner;
+public class p033_NumerosConsecutivos {
+
+    public static void main(String[] args) {
+        int n1, n2, n3;
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("\033[H\033[2J"); System.out.flush();
+        System.out.print("Escribe 3 números enteros cualquiera para verificar si son consecutivos o no.\n ");
+        System.out.print("\nDame el primer número: "); n1 = obj.nextInt();
+        System.out.print("\nDame el segundo número: "); n2 = obj.nextInt();
+        System.out.print("\nDame el tercer número: "); n3 = obj.nextInt();
+
+        if (n1+1==n2) {
+            if (n2+1==n3) {
+                System.out.println(String.format("Los números %d - %d - %d son números consecutivos.", n1, n2, n3));
+            }
+            else {
+                System.out.println(String.format("\nERROR : los números %d - %d - %d no son números consecutivos.", n1, n2, n3));
+                obj.close();
+            }
+        }
+        else {
+            System.out.println(String.format("\nERROR : los números %d - %d - %d no son números consecutivos.", n1, n2, n3));
+        }
+    }
+}
